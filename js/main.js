596cdfacde34fdc8fc3d2f761ede50fd45c27a75
Ryan Connaughton <hello@ryanconnaughton.com>
2021-11-13 12:11:08 +0000

    Bunch of styling and labelling updates

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-console.log('I ran');
+const avoidCrawlerObu = 'foo',
+  domain = 'bar.com',
+  element = document.getElementById('email');
+
+element.innerHTML = user + '@' + domain;
+//OR
+//'<a href="mailto:' + user + '@' + domain + '">Email</a>'
