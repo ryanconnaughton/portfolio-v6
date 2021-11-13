60516ea9883d632725e7bcc1d60e3871481e7652
Ryan Connaughton <hello@ryanconnaughton.com>
2021-11-13 15:34:24 +0000

    Added reveal email js to avoid crawlers

--- js/main.js
+++ js/main.js
@@ -1,7 +1,32 @@
-const avoidCrawlerObu = 'foo',
-  domain = 'bar.com',
-  element = document.getElementById('email');
+// HELPERS
+function bindToNode(node, name, fn) {
+  node[name] = fn.bind(node);
+}
 
-element.innerHTML = user + '@' + domain;
-//OR
-//'<a href="mailto:' + user + '@' + domain + '">Email</a>'
+// DATA
+function getEmail() {
+  const prefix = 'hello';
+  const domain = 'ryanconnaughton';
+  const suffix = '.com';
+
+  return prefix + '@' + domain + suffix;
+}
+
+// DOM
+const elEmail = document.getElementById('cta-email');
+elEmail.addEventListener('click', revealEmail);
+
+// CTRLERS
+function revealEmail() {
+  const emailStr = getEmail();
+
+  const elNew = document.createElement('h4');
+  elNew.className = '--primary';
+  elNew.innerHTML = emailStr;
+
+  // TODO: add copy/mail to buttons
+  // const elCopyBtn = document.createElement('button');
+  // const elMailToBtn = document.createElement('button');
+
+  elEmail.replaceChild(elNew, elEmail.childNodes[1]);
+}
